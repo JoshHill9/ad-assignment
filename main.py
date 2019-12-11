b9c1c6071a0bc9f74cd61b820b68a0aeeb947e10
JoshHill9 <joshuahill00@yahoo.co.uk>
2019-12-11 23:52:24 +0000

    Reformat of main.py
    Not working, needs development.
    
    Looking into better structure for larger python applications.

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from google.appengine.api import users
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(debug=True)
+            
+args = {}
+
+def displayPage(pageName = "home"):
+    if pageName:
+        return render_template("views/" + pageName + ".html", args)
+
 @app.route('/')
 def home():
-    return render_template("views/home.html", active="home")
+    args["active"] = "home"
+    return displayPage("home")
 
 @app.route('/about')
 def about():
-    return render_template("views/about.html", title="About", active="about")
+    args["title"] = "About"
+    args["active"] = "about"
+    return displayPage("about")
 
 @app.route('/login')
 def login():
-    return render_template("views/login.html", active="login")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    args["active"] = "login" #More login information required
+    return displayPage("login")
