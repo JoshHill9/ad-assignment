03fa7ded7d736e6701867af571b80f0d0f522099
JoshHill9 <joshuahill00@yahoo.co.uk>
2019-12-12 16:37:43 +0000

    User Model Implemented
    Login page and user model implemented.
    
    Still not working...

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash
 from google.appengine.api import users
 
 import sys
 sys.path.append('lib')
+sys.path.append('models')
 
-import models
+import User
+from wtforms_appengine.ndb import model_form
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "TestKey"
@@ -12,8 +14,7 @@ app.config["SECRET_KEY"] = "TestKey"
 args = {}
 
 def displayPage(pageName = "home"):
-    if pageName:
-        return render_template("views/" + pageName + ".html", args=args)
+    return render_template("views/" + pageName + ".html", args=args)
 
 @app.route("/")
 def home():
@@ -26,18 +27,40 @@ def about():
     args["active"] = "about"
     return displayPage("about")
 
-@app.route("/login")
+LoginForm = model_form(User.User)
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    args["active"] = "login"
-    args["title"] = "Login"
-    args["user"] = models.isUserLoggedIn()
-    return displayPage("login")
+    if request.method == "GET":
+        args["active"] = "login"
+        args["title"] = "Login"
+        user = User.getCurrentUser()
+        if user:
+            args["user"] = user
+            args["logged_in"] = True
+            return redirect('/account')
+        args["loginForm"] = LoginForm()
+        args["logged_in"] = False
+        return displayPage('login')
+    else:
+        us = request.form["username"]
+        p = request.form["password"]
+        em = request.form["email"]
+        newUser = User.User(username=us, email=em, password=p)
+        newUser.put()
+        return displayPage('account')
+
 
 @app.route("/register")
 def register():
     args["active"] = "register"
     args["title"] = "Register"
-    return displayPage("register")
+    return displayPage('register')
+
+@app.route('/account')
+def account():
+    return displayPage('account')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
