1f58cec59b08bff7b9452c807e5c4bb673861d85
JoshHill9 <joshuahill00@yahoo.co.uk>
2019-12-12 08:46:39 +0000

    Flask Routing Fixed
    Arguments can now be passed to all pages to allow for logged in user tracking etc.

--- main.py
+++ main.py
@@ -2,28 +2,36 @@ from flask import Flask, render_template
 from google.appengine.api import users
 
 app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-            
+app.config["SECRET_KEY"] = "TestKey"
+    
 args = {}
 
 def displayPage(pageName = "home"):
     if pageName:
-        return render_template("views/" + pageName + ".html", args)
+        return render_template("views/" + pageName + ".html", args=args)
 
-@app.route('/')
+@app.route("/")
 def home():
     args["active"] = "home"
     return displayPage("home")
 
-@app.route('/about')
+@app.route("/about")
 def about():
     args["title"] = "About"
     args["active"] = "about"
     return displayPage("about")
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    args["active"] = "login" #More login information required
+    args["active"] = "login"
+    args["title"] = "Login"
     return displayPage("login")
+
+@app.route("/register")
+def register():
+    args["active"] = "register"
+    args["title"] = "Register"
+    return displayPage("register")
+
+if __name__ == "__main__":
+    app.run(debug=True)
