073756e81b3f2f52fcca58e0467b72a786cf4969
JoshHill9 <joshuahill00@yahoo.co.uk>
2019-12-19 15:57:15 +0000

    Custom Login Implemented
    PyBcrypt added
    Register and Login forms completed
    Login and hashing functionality added
    (Next: Add user sessions)

--- forms.py
+++ forms.py
@@ -1,4 +1,7 @@
-from flask_wtf import FlaskForm
+import sys
+sys.path.append('lib')
+
+from flask_wtf import FlaskForm, csrf
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
@@ -10,3 +13,11 @@ class RegistrationForm(FlaskForm):
     confirmPassword = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
 
     submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    login = SubmitField("Login")
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, redirect, request, flash
-from google.appengine.api import users
-from forms import RegistrationForm
-
 import sys
 sys.path.append('lib')
 sys.path.append('models')
 
+from flask import Flask, render_template, redirect, request, flash, url_for
+from google.appengine.api import users
+from forms import RegistrationForm, LoginForm
+from pybcrypt import bcrypt
+
 import User
-import forms
-from wtforms_appengine.ndb import model_form
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "TestKey"
@@ -16,50 +15,71 @@ app.config["SECRET_KEY"] = "TestKey"
 args = {}
 
 def displayPage(pageName = "home"):
+    args["active"] = pageName
+    args["user"] = User.getCurrentUser()
+
     return render_template("views/" + pageName + ".html", args=args)
 
 @app.route("/")
 def home():
-    args["active"] = "home"
     return displayPage("home")
 
 @app.route("/about")
 def about():
-    args["active"] = "about"
     args["title"] = "About"
-    return displayPage("about")
 
-LoginForm = model_form(User.User)
+    return displayPage("about")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
-        args["active"] = "login"
-        args["title"] = "Login"
-        user = User.getCurrentUser()
-        if user:
-            args["user"] = user
-            args["logged_in"] = True
-            return redirect('/account')
-        args["loginForm"] = LoginForm()
-        args["logged_in"] = False
-        return displayPage('login')
-    else:
-        us = request.form["username"]
-        p = request.form["password"]
-        em = request.form["email"]
-        newUser = User.User(username=us, email=em, password=p)
-        newUser.put()
-        return displayPage('account')
+    args["title"] = "Login"
+    args["user_glogin"] = User.getUserLoginURL()
+    args["user_glogout"] = User.getUserLogoutURL()
+
+    args["loginForm"] = LoginForm()
+
+    if args["loginForm"].validate_on_submit():
+
+        username = args["loginForm"].username.data
+        loginUser = User.findUser(username)
+        if loginUser["new"] == True:
+            flash("Sorry, this account does not exist", 'danger')
+        else:
+            loginUser = loginUser["user"]
+            checkPw = bcrypt.hashpw(args["loginForm"].password.data, loginUser.password)
+            if checkPw == loginUser.password:
+                flash("Successfully logged in as " + username + "!", 'success')
+            else:
+                flash("Sorry, the login information provided was not correct", 'danger')
+
+    return displayPage("login")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    args["active"] = "register"
     args["title"] = "Register"
 
-    args["form"] = RegistrationForm()
+    args["registerForm"] = RegistrationForm()
 
+    if args["registerForm"].validate_on_submit():
+        name = args["registerForm"].username.data
+        userExists = User.findUser(name)
+        if userExists["new"] == False:
+            flash("Sorry, the username [" + name + "] is already taken!", 'danger')
+        else:
+            newAcc = userExists["user"]
+            try:
+                newAcc.username = args["registerForm"].username.data
+                newAcc.email = args["registerForm"].email.data
+
+                hP = bcrypt.hashpw(args["registerForm"].password.data, bcrypt.gensalt(6))
+                newAcc.password = hP
+
+                newAcc.put()
+
+                flash("Your account [" + name + "] has been created!", 'success')
+            except ValueError:
+                pass
     return displayPage("register")
 
 if __name__ == "__main__":
