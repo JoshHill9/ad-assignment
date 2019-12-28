3c98454b5a61813e0f9b59858895c765fee7d4e0
JoshHill9 <joshuahill00@yahoo.co.uk>
2019-12-28 17:06:14 +0000

    Task Queue & Task Module implemented
    Added Module to handle Task Queues for creating and deleting previous search results.
    
    Optimised search results to check for datastore values first
    
    Added results search_date to display when the results are from.

--- controllers/SearchController.py
+++ controllers/SearchController.py
@@ -1,5 +1,36 @@
-import requests, json
+import sys
+sys.path.append("models")
 
+from google.appengine.api import modules
+import requests, json, ast
+import SearchResult
+
+# Find Search Result in Datastore by Entity Key
+def get_search_result(term, location):
+    return SearchResult.get(term, location)
+
+# Create new Search Result in Datastore
+def create_search_result(term, location, result):
+    return SearchResult.create(term, location, result)
+
+# Returns 'Expired' if SearchResult has been stored for more than 1 day.
+# Returns None if SearchResult is < 1 day old
+def check_result_expiry(term, location):
+    return SearchResult.is_expired(term, location)
+
+# Deletes Search Result by Key. Key is formed from the 'term' and 'location'
+def delete_search_result(term, location):
+    SearchResult.delete(term, location)
+
+def format_saved_result(result):
+    return ast.literal_eval(result)
+
+def queue_search_task(params = {}):
+    if params:
+        module_url = "http://" + modules.get_hostname(module="task-be") + "/create_task"
+        response = requests.post(module_url, data=params)
+
+# Perform Utelly API search
 def perform_search(search_term, search_location):
     # Formats Request for Utelly API
     api_url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup"
